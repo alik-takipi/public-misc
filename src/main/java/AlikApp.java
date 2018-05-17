e1cad479fa32a57973863d15f75abf2da6653065
Alik Elzin <alik.elzin@takipi.com>
2018-05-17 17:57:08 +0300

    New exception.

--- src/main/java/AlikApp.java
+++ src/main/java/AlikApp.java
@@ -6,7 +6,7 @@ public class AlikApp
     public String getGreeting() throws Exception
     {
 //        return "Hello world.";
-        throw new AlikException69();
+        throw new AlikException70();
     }
 
     public static void main(String[] args) throws InterruptedException {
---
+++ src/main/java/AlikException70.java
@@ -0,0 +1,6 @@
+public class AlikException70 extends Exception
+{
+	public AlikException70() {
+		super("OverOps, do you see me ? " + AlikException70.class.getSimpleName());
+	}
+}
