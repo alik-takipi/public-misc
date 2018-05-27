da675e64ea75dababe8b26f0c5fa8606e667f7be
Alik Elzin <alik.elzin@takipi.com>
2018-05-27 16:08:36 +0300

    Alik tests.

--- src/main/java/AlikException75.java
+++
@@ -1,6 +0,0 @@
-public class AlikException75 extends Exception
-{
-	public AlikException75() {
-		super("OverOps, do you see me ? " + AlikException75.class.getSimpleName());
-	}
-}
---
+++ src/main/java/AlikException76.java
@@ -0,0 +1,6 @@
+public class AlikException76 extends Exception
+{
+	public AlikException76() {
+		super("OverOps, do you see me ? " + AlikException76.class.getSimpleName());
+	}
+}
