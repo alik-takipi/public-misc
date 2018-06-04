1e17ccd75353a040477a057d01bbf7474e39ea36
Alik Elzin <alik.elzin@takipi.com>
2018-06-04 17:13:50 +0300

    Advance counter

--- src/main/java/AlikApp.java
+++ src/main/java/AlikApp.java
@@ -6,7 +6,7 @@ public class AlikApp
     public String getGreeting() throws Exception
     {
 //        return "Hello world.";
-        throw new AlikException107();
+        throw new AlikException108();
     }
 
     public static void main(String[] args) throws InterruptedException {
---
+++ src/main/java/AlikException108.java
@@ -0,0 +1,6 @@
+public class AlikException108 extends Exception
+{
+	public AlikException108() {
+		super("OverOps, do you see me ? " + AlikException108.class.getSimpleName());
+	}
+}
