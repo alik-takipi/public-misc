51260ab73c0900f7bac1b7b73a506bb82a72b944
Alik Elzin <alik.elzin@takipi.com>
2018-06-05 08:54:57 +0300

    Advance counter

--- src/main/java/AlikException131.java
+++
@@ -1,6 +0,0 @@
-public class AlikException131 extends Exception
-{
-	public AlikException131() {
-		super("OverOps, do you see me ? " + AlikException131.class.getSimpleName());
-	}
-}
---
+++ src/main/java/AlikException132.java
@@ -0,0 +1,6 @@
+public class AlikException132 extends Exception
+{
+	public AlikException132() {
+		super("OverOps, do you see me ? " + AlikException132.class.getSimpleName());
+	}
+}
